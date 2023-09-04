f273f403059e73605e066aa4a59e73df7a08dfa3
Roxana <roxanaradutza@gmail.com>
2023-09-04 16:22:29 +0200

    hgf

---
+++ rn-pokedex/App.tsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { ContextProvider } from './src/context/Context';
+import AppNavigator from './src/navigation/AppNavigation';
+
+
+export default function App() {
+  
+  return (
+    <ContextProvider>
+    <NavigationContainer>
+        <AppNavigator />
+     </NavigationContainer>
+ </ContextProvider>
+  );
+}
